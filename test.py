82d7f43a124834653a902d3423719f049c06fe6b
mr zaza <hamza.yaya121@gmail.com>
2020-10-21 13:44:24 +0100

    [ADD] Add the test file

---
+++ test.py
@@ -0,0 +1,38 @@
+import unittest
+from main import overlap
+from main import find_overlaps
+
+class TestLineOverlap(unittest.TestCase):
+
+    @classmethod
+    def setUpClass(cls):
+        cls.l1 = (1, 8)
+        cls.l2 = (2, 6)
+
+        cls.l3 = (10, 20)
+        cls.l4 = (1, 9)
+
+        cls.l5 = (15, 20)
+        cls.l6 = (1, 15)
+
+        cls.to_test = {
+            'A': (3, 6),
+            'B': (1, 8),
+            'C': (15, 11),
+            'D': (6, 9)}
+        cls.response = [('A', 'B'), ('A', 'D'), ('B', 'D')]
+
+    def test_overlap_ok(self):
+
+        self.assertTrue(overlap(self.l1, self.l2))
+        self.assertTrue(overlap(self.l5, self.l6))
+
+    def test_overlap_not_ok(self):
+        self.assertFalse(overlap(self.l3, self.l4))
+
+    def test_find_overlaps_not_ok(self):
+
+        self.assertEqual(find_overlaps(self.to_test), self.response)
+
+if __name__ == '__main__':
+    unittest.main()
